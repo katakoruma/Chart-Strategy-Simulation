aa842fd247a9ec5503662f367eb4e72909f5b448
katakoruma <leon.kerner@rwth-aachen.de>
2025-03-10 00:09:37 +0100

    Update parameters

--- run_msci_compare.py
+++ run_msci_compare.py
@@ -2,31 +2,42 @@ import numpy as np
 import matplotlib.pyplot as plt 
 from performance_simulation import *
 
+#General parameters
 dt = 15
-time = 261 * 1
+time = 261 * 12
+initial_capital = 100
 
-
+#Data parameters
 year = 0
-limit = slice(year*time, (year+1)*time)
+limit = slice(year*261, year*261 + time)
 
 
+#Swing tade parameters
+trades = 20 * 7
+hold_time = 30
+time_after_reversel = 0
+smooth_period = 40
 
-sim = ChartImport(time=time, dt=dt)
+trade_coast = 1
+spread = 0.005
 
-sim.load_data(limit=limit)
-data = sim.import_data_np
+
+ci = ChartImport(time=time, dt=dt, initial_capital=initial_capital)
+
+ci.load_data(limit=limit)
+performance = ci.performance
 
 #trade_dates_random = np.array([[0, 1, 51, 56, 62, 75, 93, 123, 146, 158, 160, 175, 179, 200, 206, 211, 236, 241, 245, 257]])
 
-random_swing_performance_analyse, trade_dates_random = sim.random_swing_trade_ana(set='data', trades=50, trade_dates=None)
-swing_performance_analyse, trade_dates = sim.swing_trade_ana(set='data', smooth_period=1, trades=100, hold_time=14, time_after_reversel=0, trade_dates=None)
+random_swing_performance_analyse, trade_dates_random  = ci.random_swing_trade_ana(performance, trades=trades, trade_dates=None, trade_coast=trade_coast, spread=spread)
+swing_performance_analyse, trade_dates = ci.swing_trade_ana(performance, smooth_period=smooth_period, trades=trades, hold_time=hold_time, time_after_reversel=time_after_reversel, trade_dates=None, trade_coast=trade_coast, spread=spread)
 
-print("Buy and hold return: ", data[-1])
+print("Buy and hold return: ", performance[-1])
 print("Random swing trade return analyse: ", random_swing_performance_analyse[-1])
 print("Swing trade return analyse: ", swing_performance_analyse[-1])
 
 
-plt.plot(data, label="Buy and hold")
+plt.plot(performance, label="Buy and hold")
 plt.plot(swing_performance_analyse, label="Swing trade analyse")
 plt.plot(random_swing_performance_analyse, label="Random swing trade analyse")
 plt.axhline(1, color="black", linestyle="--")   
--- run_simulation.py
+++ run_simulation.py
@@ -3,29 +3,41 @@ import numpy as np
 import matplotlib.pyplot as plt 
 from performance_simulation import *
 
+#General parameters
+dt = 15
+time = 261 * 5
+initial_capital = 100
 
-yearly_return = 1.07
+
+#Simulation parameters
+yearly_return = 1.07**5
 
 daily_return = 1.001
 daily_loss = 1 - 0.01
 
-
 gain_phase = 0.7
 loss_phase = 1 - gain_phase
-
-dt = 15
-time = 261
 
 mode = "constant_timesteps"
 #mode = "constant_gain"
 
 
-sim = ChartSimulation(yearly_return, daily_return, daily_loss, gain_phase, loss_phase, mode, time, dt)
+#Swing tade parameters
+trades = 20 * 5
+hold_time = 90
+time_after_reversel = 0
+smooth_period = 1
+
+trade_coast = 1
+spread = 0.005
+
+
+sim = ChartSimulation(yearly_return, daily_return, daily_loss, gain_phase, loss_phase, mode, time, dt, initial_capital)
 
 performance, phase = sim.simulate_performance()
 
-random_swing_performance_analyse, trade_dates_random  = sim.random_swing_trade_ana(performance, trades=10, trade_dates=None, trade_coast=0.1, spread=0.)
-swing_performance_analyse, trade_dates = sim.swing_trade_ana(performance, smooth_period=1, trades=20, hold_time=14, time_after_reversel=0, trade_dates=None, trade_coast=0.1, spread=0.)
+random_swing_performance_analyse, trade_dates_random  = sim.random_swing_trade_ana(performance, trades=trades, trade_dates=None, trade_coast=trade_coast, spread=spread)
+swing_performance_analyse, trade_dates = sim.swing_trade_ana(performance, smooth_period=smooth_period, trades=trades, hold_time=hold_time, time_after_reversel=time_after_reversel, trade_dates=None, trade_coast=trade_coast, spread=spread)
 
 print("Buy and hold return: ", performance[-1])
 print("Random swing trade return analyse: ", random_swing_performance_analyse[-1])
@@ -35,12 +47,14 @@ print("Best return: ", performance[0] * daily_return**(np.sum(phase == 1)) )
 plt.plot(performance, label="Buy and hold")
 plt.plot(swing_performance_analyse, label="Swing trade analyse")
 plt.plot(random_swing_performance_analyse, label="Random swing trade analyse")
-plt.axhline(1, color="black", linestyle="--")   
+#plt.axhline(1, color="black", linestyle="--")   
 
 plt.xlabel("Time")
 plt.ylabel("Performance")
 
 plt.grid()
 plt.legend()
+
+#plt.yscale("log")
 
 plt.show()
